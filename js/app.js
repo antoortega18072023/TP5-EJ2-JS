472a5c853f95bb972284be3940e4c969771a0da4
Antonella Ortega <antonellaportega@gmail.com>
2023-11-28 01:27:41 -0300

    creacion de vaiables

---
+++ js/app.js
@@ -0,0 +1,7 @@
+//  Variables necesarias
+const btnEnviarDatos = document.querySelector('.btn-success')
+const btnMostrarDatos = document.querySelector('.btn-primary')
+const btnGeneracion = document.querySelector('.btn-warning')
+const btnMayorDeEdad = document.querySelector('.btn-danger')
+const formularioDatos = document.querySelector('form')
+let listaUsuarios = []
